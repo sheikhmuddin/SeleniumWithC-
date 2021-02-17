0877622611f196bd2ddfc70666ccbb52ff2731d7
sheikhmuddin <78605865+sheikhmuddin@users.noreply.github.com>
2021-02-17 14:06:57 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("Ducks");
+            driver.FindElement(By.ClassName("gNO89b"));
+            Console.WriteLine(driver.Title);
+            driver.Close();
+        }
+    }
+}
